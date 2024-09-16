dc9aa2286634db6e4bec8f7a12321d83e9078ce2
Chien Nguyen <chiennguyen1702@gmail.com>
2024-09-16 07:45:10 +0000

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stay-gold-ltd/wp-mail-smtp-pro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6804113fab0c1b1aa9e7a2f2b56273de5854ef37',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'stay-gold-ltd/wp-mail-smtp-pro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6804113fab0c1b1aa9e7a2f2b56273de5854ef37',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
